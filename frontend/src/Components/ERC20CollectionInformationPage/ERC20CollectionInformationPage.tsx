57590fe15950f55270cddf3765fc2d76b7ffd9fd
Abdul <abdullahmd326@gmail.com>
2023-04-16 00:57:42 -0400

    ERC20 Collection Page front end routes complete

--- frontend/src/Components/ERC20CollectionInformationPage/ERC20CollectionInformationPage.tsx
+++ frontend/src/Components/ERC20CollectionInformationPage/ERC20CollectionInformationPage.tsx
@@ -1,20 +1,130 @@
 import { FC, useState, useRef, FormEvent } from 'react';
-import React from 'react';
 import Alert from '../Alert/Alert';
 import { useNavigate } from 'react-router';
+import axios from 'axios';
 
 // Adding ERC20 Collection Page for Analytics
 const ERC20CollectionInformationPage: FC = () => {
     const [setAlert, updateAlert] = useState<boolean>(false);
     const tokenAddress = useRef<HTMLInputElement>(null);
-    const navigate = useNavigate();
+    const [setPrice, updateERC20Price] = useState(null);
     
-    const clearHandler = () => {
+    const [ERC20Transfers, updateTransfers] = useState({
+        information: null
+    });
 
+    const [ERC20Information, updateERC20Information] = useState({
+        information: null
+    });
+
+    const navigate = useNavigate();
+
+    // Node Server endpoints
+    const NODE_SERVER_URL = 'http://localhost:5000';
+    const NODE_SERVER_TOKEN_PRICE_ENDPOINT = '/matic-erc20-collection-information';
+    const NODE_SERVER_TOKEN_TRANSFER_ENDPOINT = '/matic-erc20-collection-transfers';
+    const NODE_MATIC_PRICE_ENDPOINT = '/get-matic-price';
+
+    const clearHandler = () => {
+        updateAlert(false);
+    }
+
+    const alertHandler = () => {
+        updateAlert(true); // Add alerts if any don't exist, and clear all information for analytics
+        updateTransfers((prevState) => {
+            return {
+                ...prevState,
+                information: null
+            }
+        });
+
+        updateERC20Price(null);
+        updateERC20Information((prevState) => {
+            return {
+                ...prevState,
+                information: null
+            }
+        });
     }
 
     const formHandler = (e: FormEvent<HTMLFormElement>) => {
+        // Set options for fetch and flight responses
+        const options = {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify({ address : tokenAddress.current!.value }),
+            headers: {
+                'content-type' : 'application/json', 
+                'accept': 'application/json',
+            }
+        }
 
+        if (tokenAddress.current!.value.length === 42 && tokenAddress.current!.value.substring(0, 2) === '0x'){
+            axios.post(NODE_SERVER_URL + NODE_SERVER_TOKEN_TRANSFER_ENDPOINT, options) // ERC20 endpoint for retrieving information related to transfers
+            .then(response => {
+                if (response.status !== 200){
+                    updateAlert(true);
+                    alertHandler();
+                }
+                else {
+                    if (response.status === 200 && response.data.information.result.length === 0){ // If empty, remove information
+                        updateAlert(false);
+                        alertHandler();
+                    }
+                    else {
+                        updateAlert(false); // Remove alerts if any exist
+                        updateTransfers((prevState) => {
+                            return {
+                                ...prevState,
+                                information: response.data.information.result
+                            }
+                        });
+                    }
+                }
+            })
+            .catch(() => {
+                alertHandler();
+            });
+
+            axios.post(NODE_SERVER_URL + NODE_SERVER_TOKEN_PRICE_ENDPOINT, options) // ERC20 endpoint for retrieving information related to price
+                .then(response => {
+                    if (response.status !== 200){
+                        alertHandler();                    
+                    }
+                    else {
+                        if (response.status === 200 && Object.keys(response.data.information).length === 0){ // If empty, clear price
+                            alertHandler();                            
+                        }
+                        else {
+                            updateERC20Price(response.data.information.usdPrice);
+                        }
+                    }
+                })
+                .catch(() => {
+                    alertHandler();
+                });
+            
+            axios.get(NODE_SERVER_URL + NODE_MATIC_PRICE_ENDPOINT) // Retrieve price of ERC20 coin from coingecko
+            .then(response => {
+                if (response.status === 200 && Object.keys(response).length > 0) { // Check results to see if they match criteria
+                    updateERC20Information((prevState) => {
+                        return {
+                            ...prevState,
+                            information: response.data
+                        }
+                    })
+                }
+                else {
+                    alertHandler();
+                }
+            })
+            .catch(() => {
+                alertHandler();
+            });
+        }
+        else {
+            alertHandler();
+        }
     }
 
     return (
