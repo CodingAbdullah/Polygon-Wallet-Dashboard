e654f5d1994ee6b4a596d45781a763d3cdadba6e
Abdul <abdullahmd326@gmail.com>
2023-05-21 23:49:52 -0400

    Adding ERC721TransfersInfoTable

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
 # Code for provisioning AWS resources and IAC (Infrastructure As Code) will go here
+# Terraform special configuration block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configuring the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
