448398351593bbeedd57b481775758b2cdd55985
Abdul <abdullahmd326@gmail.com>
2023-06-02 23:59:20 -0400

    ERC721Information Refactoring, endpoints to be completed

--- frontend/src/Components/ERC721CollectionInformationPage/ERC721CollectionInformationPage.tsx
+++ frontend/src/Components/ERC721CollectionInformationPage/ERC721CollectionInformationPage.tsx
@@ -7,19 +7,19 @@ import axios from 'axios';
 
 // Adding ERC20 Collection Page for Analytics
 const ERC20CollectionInformationPage: FC = () => {
-    const [setAlert, updateAlert] = useState<boolean>(false);
-    const [emptyAlert, updateEmptyAlert] = useState<boolean>(false);
-    const tokenAddress = useRef<HTMLInputElement>(null);
-    const [collectionInformation, updateCollectionInformation] = useState<ERC721CollectionInformationType>();
-    const [collectionTransfers, updateCollectionTransfers] = useState<ERC721CollectionTransferInformationType>();
-    const [collectionAttributes, updateCollectionAttributes] = useState(null);
-    const [collectionSales, updateCollectionSales] = useState(null);
-
     const NODE_SERVER_URL = 'http://localhost:5001';
     const MATIC_ERC721_COLLECTION_INFORMATION_ENDPOINT = '/matic-erc721-collection-information';
     const MATIC_ERC721_COLLECTION_TRANSFERS_ENDPOINT = '/matic-erc721-collection-transfers';
     const MATIC_ERC721_COLLECTION_ATTRIBUTES_ENDPOINT = '/matic-erc721-collection-attributes';
     const MATIC_ERC721_COLLECTION_SALES_ENDPOINT = '/matic-erc721-collection-sales';
+
+    const [emptyAlert, updateEmptyAlert] = useState<boolean>(false);
+    const [collectionInformation, updateCollectionInformation] = useState<ERC721CollectionInformationType>();
+    const [collectionTransfers, updateCollectionTransfers] = useState<ERC721CollectionTransferInformationType>();
+    const [collectionAttributes, updateCollectionAttributes] = useState(null);
+    const [collectionSales, updateCollectionSales] = useState(null);
+    const [setAlert, updateAlert] = useState<boolean>(false);
+    const tokenAddress = useRef<HTMLInputElement>(null);
 
     const navigate = useNavigate();
     
